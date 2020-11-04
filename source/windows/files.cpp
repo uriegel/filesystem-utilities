23f7ad694426b509cede097d6c16925c55b4b20c
Uwe Riegel <uriegel@hotmail.de>
2020-11-04 10:38:02 +0100

    GetFiles for windows

--- source/windows/files.cpp
+++ source/windows/files.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 
 vector<File_item> get_files(const wstring& directory) {
-    vector<File_item> result;
-    return result;
+    auto search_string = (directory[directory.length()-1] == L'\\' || directory[directory.length()-1] == L'/') 
+        ? directory + L"*.*"s
+        : directory + L"\\*.*"s;
+    replace(search_string.begin(), search_string.end(), L'/', L'\\'); 
+
+	vector<File_item> result;
+    WIN32_FIND_DATAW w32fd{ 0 };
+    auto ret = FindFirstFileW(search_string.c_str(), &w32fd);
+    while (FindNextFileW(ret, &w32fd) == TRUE) {
+        result.emplace_back(
+            w32fd.cFileName,
+            (w32fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY,
+            (w32fd.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN) == FILE_ATTRIBUTE_HIDDEN,
+            static_cast<uint64_t>(w32fd.nFileSizeHigh) << 32 | w32fd.nFileSizeLow,
+            convert_windowstime_to_unixtime(w32fd.ftLastWriteTime)
+        );
+    }
+	FindClose(ret);
+	return result;
 }
