0f4ff9d6e9f0cbb97caaf05dc3c86ce444c25761
Uwe Riegel <uriegel@hotmail.de>
2020-11-04 17:24:49 +0100

    Performance chack on linux

--- dev/index.js
+++ dev/index.js
@@ -7,7 +7,19 @@ const addon = require('bindings')('filesystem-utilities')
 
     const files = await addon.getFiles("build")
     console.log("files", files)
+
+    await runProfileTests()
 })()
+
+async function runProfileTests() {
+
+    const start = process.hrtime.bigint()
+    const files = await addon.getFiles("/media/Speicher/bilder/2017/Abu Dabbab")
+    const sortedFiles = files.sort((a, b) => -a.name.localeCompare(b.name))
+    let end = process.hrtime.bigint()
+    console.info(`Execution time getFiles: ${((end - start)/ BigInt(1000000.0))} ms`)
+    console.log("files", files.length)
+}
 
 // TODO: Windows version without ..
 // TODO: check timing 4000 files
