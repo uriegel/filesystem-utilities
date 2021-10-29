72c03bda0acc33e7d4fd3c049a30614a3af99989
uriegel <uriegel@hotmail.de>
2021-10-29 19:35:59 +0200

    gtk_init bug in electron

--- source/linux/globals.cpp
+++ source/linux/globals.cpp
@@ -3,9 +3,8 @@
 GtkIconTheme* default_theme;
 
 void init() {
-    return;
-    int argc{0};
-    gtk_init(&argc, (char***)nullptr);
+    // int argc{0};
+    // gtk_init(&argc, (char***)nullptr);
     default_theme = gtk_icon_theme_get_default();
 }
 
