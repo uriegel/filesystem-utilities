0e60e6a0218b3f45335d98cbace3d208bd49dd52
uriegel <uriegel@hotmail.de>
2021-10-31 08:07:47 +0100

    Test without setLocale and gtk_init

--- source/linux/globals.cpp
+++ source/linux/globals.cpp
@@ -10,9 +10,9 @@ void check_init()
 {
     lock_guard<mutex> lockGuard(m);
     if (!is_initialized) {
-        gtk_disable_setlocale();
-        int argc{0};
-        gtk_init(&argc, (char***)nullptr);
+        //gtk_disable_setlocale();
+        //int argc{0};
+        //gtk_init(&argc, (char***)nullptr);
         default_theme = gtk_icon_theme_get_default();
         is_initialized = true;
     }
