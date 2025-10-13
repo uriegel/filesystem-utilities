d7a27b530a36207886df9730d52950dccc32b4c4
Uwe Riegel <uriegel@hotmail.de>
2025-10-13 14:54:07 +0200

    get icon initially gets empty data

--- source/linux/icon.cpp
+++ source/linux/icon.cpp
@@ -23,40 +23,46 @@ void checkInitializeIcons() {
 
 //bool svg
 std::vector<char> get_icon(const std::string& extension) {
-    //if (extension.length() > 0) {
-        auto type = g_content_type_guess(extension.c_str(), nullptr, 0, nullptr);
-        auto icon = g_content_type_get_icon(type);
+    auto type = g_content_type_guess(extension.c_str(), nullptr, 0, nullptr);
+    auto icon = g_content_type_get_icon(type);
+    if (type)
+        g_free(type);
+    auto names = g_themed_icon_get_names((GThemedIcon*)icon);
+    auto icon_info = gtk_icon_theme_choose_icon(default_theme, (const gchar**)names, 16, (GtkIconLookupFlags)0);
+    if (icon_info == nullptr)
+        cout << "icon_info is null" << endl;
+    if (icon)
+        g_object_unref(icon);
+    auto filename = gtk_icon_info_get_filename(icon_info);
+    auto filename_char = (char *)filename;
+    if (icon_info == nullptr && filename_char != nullptr)
+        cout << "icon file" << " - " << filename_char << endl;
+    if (filename_char == nullptr)
+        cout << "icon file is null" << endl;
+
+    if (filename_char != nullptr) {
+        auto affe = strncmp(filename_char, "/org/gkt", 8);
+        cout << "strncmp" << affe << endl;
+    }
+    if (filename_char == nullptr || strncmp(filename_char, "/org/gkt", 8) == 0) {
+        cout << "try again" << endl;
+        this_thread::sleep_for(chrono::milliseconds(200));
+        type = g_content_type_guess(extension.c_str(), nullptr, 0, nullptr);
+        icon = g_content_type_get_icon(type);
         if (type)
             g_free(type);
-        auto names = g_themed_icon_get_names((GThemedIcon*)icon);
-        auto icon_info = gtk_icon_theme_choose_icon(default_theme, (const gchar**)names, 16, (GtkIconLookupFlags)0);
+        names = g_themed_icon_get_names((GThemedIcon*)icon);
+        icon_info = gtk_icon_theme_choose_icon(default_theme, (const gchar**)names, 16, (GtkIconLookupFlags)0);
         if (icon)
             g_object_unref(icon);
-        auto filename = gtk_icon_info_get_filename(icon_info);
-        auto filename_char = (char *)filename;
-        if (strncmp(filename_char, "/org/gkt", 8) != 0) {
-            cout << "icon file" << " - " << (char *)filename << endl;
-            this_thread::sleep_for(chrono::milliseconds(500));
-            type = g_content_type_guess(extension.c_str(), nullptr, 0, nullptr);
-            icon = g_content_type_get_icon(type);
-            if (type)
-                g_free(type);
-            names = g_themed_icon_get_names((GThemedIcon*)icon);
-            icon_info = gtk_icon_theme_choose_icon(default_theme, (const gchar**)names, 16, (GtkIconLookupFlags)0);
-            if (icon)
-                g_object_unref(icon);
-            filename = gtk_icon_info_get_filename(icon_info);
-            cout << "icon file" << " - " << (char *)filename << endl;
-        }
-        ifstream input(filename, ios::binary);
-        vector<char> result(istreambuf_iterator<char>(input), {});
-        // if (icon_info)
-        //     g_object_unref(icon_info);
-        return result;
-    // } else {
-    //     vector<char> result;
-    //     return result;
-    // }
+        filename = gtk_icon_info_get_filename(icon_info);
+        cout << "icon file" << " - " << (char *)filename << endl;
+    }
+    ifstream input(filename, ios::binary);
+    vector<char> result(istreambuf_iterator<char>(input), {});
+    // if (icon_info)
+    //     g_object_unref(icon_info);
+    return result;
 }
 
 
