0c99fe16c3b3f41aca71c4b5f87bcacb22e03010
Uwe Riegel <uriegel@hotmail.de>
2025-10-30 11:10:29 +0100

    Network shares build ready

--- source/windows/network_share_worker.cpp
+++ source/windows/network_share_worker.cpp
@@ -1,7 +1,26 @@
 #include <napi.h>
+#include <windows.h>
+#include <winnetwk.h>
 #include "network_share_worker.h"
+#include "..\wstring.h"
 using namespace Napi;
 using namespace std;
+
+int make_share(const wstring& share, const wstring& name, const wstring& passwd) {
+//    [DllImport("mpr.dll")]
+
+    NETRESOURCEW nr = {
+        RESOURCE_GLOBALNET,
+        RESOURCETYPE_DISK,
+        RESOURCEDISPLAYTYPE_SHARE,
+        0,
+        (LPWSTR)share.c_str(),
+        nullptr, 
+        nullptr
+    };
+
+    return WNetAddConnection2W(&nr, passwd.c_str(), name.c_str(), 0);
+}
 
 class Netwotk_share_worker : public AsyncWorker {
 public:
@@ -13,7 +32,7 @@ public:
     , passwd(passwd) {}
     ~Netwotk_share_worker() {}
 
-    void Execute () { versions = get_versions(input, cancellation); }
+    void Execute () { make_share(share, name, passwd); }
 
     void OnOK();
 
@@ -24,8 +43,11 @@ private:
     wstring share;
     wstring name;
     wstring passwd;
-    vector<VersionInfoResult> versions;
 };
+
+void Netwotk_share_worker::OnOK() {
+    
+}
 
 Value AddNetworkShare(const CallbackInfo& info) {
     auto share = info[0].As<WString>().WValue();
