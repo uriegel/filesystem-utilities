1782cd2960a7c02f264998297351db17bfeb90ab
Uwe Riegel <uriegel@hotmail.de>
2025-10-31 20:13:30 +0100

    3 more images

--- filesystem-utilities.d.ts
+++ filesystem-utilities.d.ts
@@ -130,6 +130,9 @@ declare module 'filesystem-utilities' {
      *  * 'folder-open'
      *  * 'user-home'
      *  * 'go-up'
+     *  * 'network-server'
+     *  * 'starred'
+     *  * 'android'
      * @result The icon as binary data
      */
     function getIconFromName(name: string): Promise<Buffer>
